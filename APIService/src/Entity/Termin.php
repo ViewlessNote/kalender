85fd03bf0a68abe07da4642c970d3d611ccd55bd
jf <Jonas.Feldhaus@icloud.com>
2024-11-15 10:00:33 +0100

    Dependency Reposetory

---
+++ APIService/src/Entity/Termin.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TerminRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TerminRepository::class)]
+class Termin
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Start = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Ende = null;
+
+    #[ORM\Column]
+    private ?bool $Aufgabe = null;
+
+    #[ORM\Column]
+    private ?bool $Aktiv = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Details = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->Start;
+    }
+
+    public function setStart(\DateTimeInterface $Start): static
+    {
+        $this->Start = $Start;
+
+        return $this;
+    }
+
+    public function getEnde(): ?\DateTimeInterface
+    {
+        return $this->Ende;
+    }
+
+    public function setEnde(\DateTimeInterface $Ende): static
+    {
+        $this->Ende = $Ende;
+
+        return $this;
+    }
+
+    public function isAufgabe(): ?bool
+    {
+        return $this->Aufgabe;
+    }
+
+    public function setAufgabe(bool $Aufgabe): static
+    {
+        $this->Aufgabe = $Aufgabe;
+
+        return $this;
+    }
+
+    public function isAktiv(): ?bool
+    {
+        return $this->Aktiv;
+    }
+
+    public function setAktiv(bool $Aktiv): static
+    {
+        $this->Aktiv = $Aktiv;
+
+        return $this;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->Details;
+    }
+
+    public function setDetails(?string $Details): static
+    {
+        $this->Details = $Details;
+
+        return $this;
+    }
+}
---
+++ APIService/src/Repository/TerminRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+use App\Entity\Termin;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Termin>
+ */
+class TerminRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, private EntityManagerInterface $em)
+    {
+        parent::__construct($registry, Termin::class);
+    }
+    public function setNewTermin(termin $termin){
+         $this->em->persist($termin);
+         $this->em->flush();
+
+    }
+
+    //    /**
+    //     * @return Termin[] Returns an array of Termin objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Termin
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
